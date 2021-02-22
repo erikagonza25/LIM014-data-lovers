54bf9da0292ab0aec6a442329bbd116af5d09f26
Erika <erikagonza2013@gmail.com>
2021-02-22 10:42:28 -0500

    Arreglo de test Linea(229,232)

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,15 @@
 import { dataSort, filterData, filterName, filterLevel } from "../src/data.js";
 const testLol = [
   {
-    id: "Akali",
-    name: "Akali",
-    tags: ["Assassin"],
-    info: { attack: 5, defense: 3, magic: 8, difficulty: 7 },
-  },
-  {
     id: "Braum",
     name: "Braum",
     tags: ["Support", "Tank"],
     info: { attack: 3, defense: 9, magic: 4, difficulty: 3 },
+  },{
+    id: "Akali",
+    name: "Akali",
+    tags: ["Assassin"],
+    info: { attack: 5, defense: 3, magic: 8, difficulty: 7 },
   },
   {
     id: "Ahri",
@@ -114,7 +113,6 @@ const testOrderAz = [
     tags: ["Assassin"],
     info: { attack: 5, defense: 3, magic: 8, difficulty: 7 },
   },
-
   {
     id: "Alistar",
     name: "Alistar",
@@ -226,10 +224,10 @@ describe("dataSort", () => {
     expect(typeof dataSort).toBe("function");
   });
   it("deberia ordenar los personajes de la Z-A", () => {
-    expect(dataSort(testLol, "name", "za")).toEqual(testOrder);
+    expect(dataSort(testLol,"za")).toEqual(testOrder);
   });
   it("deberia ordenar los personajes de la A-Z", () => {
-    expect(dataSort(testLol, "name", "az")).toEqual(testOrderAz);
+    expect(dataSort(testLol,"az")).toEqual(testOrderAz);
   });
 });
 
@@ -269,48 +267,4 @@ describe("filterLevel", () => {
     expect(filterLevel(testLol, "dificil")).toEqual(testLevel);
   });
 });
-/*const testLolTwo = [
-  {
-    id: "Zyra",
-    name: "Zyra",
-    tags: ["Mage", "Support"],
-    info: { attack: 4, defense: 3, magic: 8, difficulty: 7 },
-  },
-  {
-    id: "Akali",
-    name: "Akali",
-    tags: ["Assassin"],
-    info: { attack: 5, defense: 3, magic: 8, difficulty: 7 },
-  },
-  {
-    id: "Braum",
-    name: "Braum",
-    tags: ["Support", "Tank"],
-    info: { attack: 3, defense: 9, magic: 4, difficulty: 3 },
-  },
-  {
-    id: "Ahri",
-    name: "Ahri",
-    tags: ["Mage", "Assassin"],
-    info: { attack: 3, defense: 4, magic: 8, difficulty: 5 },
-  },
-  {
-    id: "Aatrox",
-    name: "Aatrox",
-    tags: ["Fighter", "Tank"],
-    info: { attack: 8, defense: 4, magic: 3, difficulty: 4 },
-  },
-  {
-    id: "Zyra",
-    name: "Zyra",
-    tags: ["Mage", "Support"],
-    info: { attack: 4, defense: 3, magic: 8, difficulty: 7 },
-  },
 
-  {
-    id: "Alistar",
-    name: "Alistar",
-    tags: ["Tank", "Support"],
-    info: { attack: 6, defense: 9, magic: 5, difficulty: 7 },
-  },
-]; */
